5a45411b07375c7f81445e1229dc8bfbb918060d
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2018-12-01 12:33:25 +0000

    Day 1

---
+++ Day1/day1Naomi.py
@@ -0,0 +1,25 @@
+file = "frequency.txt"
+
+sum = 0
+freqchanges = []
+values = []
+
+f = open(file,'r')
+while True:
+    line = f.readline()
+    if not line:
+        break
+    freqchanges.append(int(line.strip()))
+
+index = 0
+values.append(0)
+while True:
+    sum+=freqchanges[index]
+    if sum in values:
+        break
+    values.append(sum)
+    index = index + 1
+    if index == len(freqchanges):
+        index = 0
+
+print(sum)
