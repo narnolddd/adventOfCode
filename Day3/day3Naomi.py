96494a514e577abb06666ee8afc6a4aff919ad8f
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2018-12-03 10:52:56 +0000

    naomi part 2

--- Day3/day3Naomi.py
+++ Day3/day3Naomi.py
@@ -1,6 +1,10 @@
+from collections import defaultdict
 import numpy as np
 
-sqcounts = np.zeros((1000,1000), dtype=int)
+sqclaims = [[[] for y in range(1000)] for x in range(1000)]
+counts = np.zeros((1000,1000),dtype=int)
+validclaims = defaultdict(lambda:True)
+data=[]
 
 input = "Day3/input.txt"
 
@@ -12,9 +16,21 @@ with open(input,'r') as f:
         parts = line.strip().split(" ")
         corner = list(map(int,parts[2].strip(":").split(",")))
         dims = list(map(int,parts[3].split("x")))
+        claimno = int(parts[0].strip("#"))
         for i in range(dims[0]):
             for j in range(dims[1]):
-                sqcounts[corner[0]+i,corner[1]+j] +=1
+                x=corner[0]+i
+                y=corner[1]+j
+                if len(sqclaims[x][y])>0:
+                    validclaims[claimno]=False
+                    for claim in sqclaims[x][y]:
+                        validclaims[claim]=False
+                sqclaims[x][y].append(claimno)
+                counts[x,y] +=1
 
-overlaps = (sqcounts>1).sum()
-print(overlaps)
+print("Number of squares with two or more claims: ")
+print((counts>1).sum())
+print("ID of valid claim: ")
+for i in range(1409):
+    if validclaims[i+1]:
+        print(i+1)
