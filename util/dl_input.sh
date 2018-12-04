fb2ec2a2fbda5036d0ebed0458922020e4945e5a
Timm Böttger <timm.boettger@qmul.ac.uk>
2018-12-04 18:37:29 +0000

    Make dl script immune to calling folder

--- util/dl_input.sh
+++ util/dl_input.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+## https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null && pwd )"
+
+echo $DIR
+
 if [  $# -ne 1 ]; then
 	echo "Usage: $0 day"
 	exit 1
 fi
 
 day=$1
-sessionid=`cat sessionid.txt`
+sessionid=`cat $DIR/sessionid.txt`
 
-mkdir -p "../Day$day"
-curl --cookie "$sessionid" "https://adventofcode.com/2018/day/$day/input" > "../Day$day/input.txt"
+mkdir -p "$DIR/../Day$day"
+curl --cookie "$sessionid" "https://adventofcode.com/2018/day/$day/input" > "$DIR/../Day$day/input.txt"
