ea42f974231faf546734ca96442c26de03ab08e2
Linh Tran <linh.tran@imperial.ac.uk>
2018-12-05 15:48:56 +0000

    submitting old ugly version with 2 pointers

--- Day5/linh05.py
+++ Day5/linh05.py
@@ -27,3 +27,29 @@ for a in alphabet:
     shortest_pol = min(shortest_pol, len(stack))
 
 print(shortest_pol)
+
+# old version with two pointers
+p1 = 0
+p2 = 1
+polymer = list(polymer)
+
+while p2 < len(polymer):
+    if polymer[p1] == mapping[polymer[p2]]:
+        polymer[p1] = '_'
+        polymer[p2] = '_'
+        p1 -= 1
+        while p1 > -1 and polymer[p1] == '_':
+            p1 -= 1
+        p2 += 1
+        # p1 < 0
+    else:
+        p1 += 1
+        p2 += 1
+        while p1 == '_':
+            p1 += 1
+    while p1 < 0 or polymer[p1] == '_':
+        p1 += 1
+    if p1 == p2:
+        p2 += 1
+
+print(len([s for s in polymer if s != '_']))
