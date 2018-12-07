cbdf317df6c554272aeec218f9f36638ce3ce18c
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2018-12-07 12:48:43 +0000

    Naomi's full solution

--- Day7/day7Naomi.py
+++ Day7/day7Naomi.py
@@ -1,23 +1,55 @@
 import re
 import networkx as nx
+from collections import defaultdict
 file = "Day7/input.txt"
 
 line = "Step (?P<src>\w{1}) must be finished before step (?P<dst>\w{1}) can begin."
 p= re.compile(line)
 
-G=nx.DiGraph()
+# Make a dependency graph of the tasks A -> B if B dependent on A
+H=nx.DiGraph()
 with open(file,'r') as f:
     for l in f:
         match = p.match(l.strip()).groupdict()
-        G.add_edge(match['src'],match['dst'])
+        H.add_edge(match['src'],match['dst'])
 
+workers = ["." for i in range(5)]
+
+# Returns a string of letter repeated 60+number of letter times
+def letter2time(letter):
+    alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for i in range(0,26):
+        if alphabet[i]==letter:
+            return letter*(61+i)+"."
+
+# Part 1 & 2 in one go
+second=0
 order=""
 while True:
-    available_tasks = [node for node in G.nodes() if G.in_degree(node)==0 ]
-    if len(available_tasks)==0:
+    for w in workers:
+        if second > 0 and w[second]== ".":
+            if w[second-1]!=".":
+                H.remove_node(w[second-1])
+    if second>0 and all([w[second]=="." and w[second-1]=="." for w in workers]):
         break
-    first = min(available_tasks)
-    order+=first
-    G.remove_node(first)
+    # if task has no indegree then it is available
+    available_tasks = [node for node in H.nodes() if H.in_degree(node)==0 and all([w[second]!= node for w in workers])]
+    if len(available_tasks)!=0:
+        sortedtasks=sorted(available_tasks)
+        for w in range(len(workers)):
+            if len(sortedtasks)==0:
+                if workers[w][second]==".":
+                    workers[w]+="."
+            else:
+                if workers[w][second]==".":
+                    workers[w]=workers[w][:-1]
+                    workers[w]+=letter2time(sortedtasks[0])
+                    order+=sortedtasks[0]
+                    sortedtasks.pop(0)
+    else:
+        for w in range(len(workers)):
+            if workers[w][second]==".":
+                workers[w]+="."
+    second+=1
 
-print(order)
+print(second-1,order)
