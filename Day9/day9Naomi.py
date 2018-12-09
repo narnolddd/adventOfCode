29a0367bf491ce4d0a1ffbd3d3926e1610577ae0
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2018-12-09 16:33:56 +0000

    part 2 complete

--- Day9/day9Naomi.py
+++ Day9/day9Naomi.py
@@ -1,5 +1,6 @@
 import re
 import numpy as np
+from collections import deque
 
 file = "Day9/input.txt"
 regex = "(?P<nplayers>\d+) players; last marble is worth (?P<nmarbles>\d+) points"
@@ -35,7 +36,28 @@ for marble in range(2,nmarbles+1):
             marblestack.insert((current+2)%len(marblestack),marble)
             current = (current+2)%(len(marblestack)-1)
             player+=1
-    if marble < 25:
-        print(marblestack, str(current), str(marblestack[current]))
+    #if marble < 25:
+        #print(marblestack, str(current), str(marblestack[current]))
+
+print(max(scores))
+
+#nmarbles = 100*nmarbles
+scores = np.zeros((nplayers),dtype=int)
+
+nmarbles = 100*nmarbles
+marblestack=deque()
+
+player = 0
+current = 0
+for marble in range(nmarbles+1):
+    if player == nplayers:
+        player=0
+    if marble > 0 and marble % 23 == 0:
+        marblestack.rotate(7)
+        scores[player]+= marble + marblestack.popleft()
+    else:
+        marblestack.rotate(-2)
+        marblestack.appendleft(marble)
+    player+=1
 
 print(max(scores))
