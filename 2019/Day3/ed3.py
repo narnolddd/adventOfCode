2e92d0902f3a13f0e479bad719f8bc96caf089b1
ederlf <ederleaofernandes@gmail.com>
2019-12-03 15:43:23 +0000

    Not nice, but does the job

---
+++ 2019/Day3/ed3.py
@@ -0,0 +1,66 @@
+from collections import namedtuple
+Point = namedtuple('Point', 'x y path steps')
+
+def distance(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+def check_colision(grid, colisions, pos, pnum, steps):
+    if pos in grid:
+        if grid[pos].path != pnum:
+            print (grid[pos], steps)
+            colisions.append( (grid[pos], Point(pos[0], pos[1], pnum, steps)) )
+    else:     
+        grid[newpos] = Point (newpos[0], newpos[1], pnum, steps)
+
+f = open('input', 'r')
+
+paths = [x for x in f.readlines()]
+
+grid = {}
+grid[(0,0)] = Point(0, 0, -1, 0)
+colisions = []
+
+pnum = 1
+for path in paths:
+    moves = path.strip('\n').split(',')
+    pos = (0, 0)
+    steps = 0
+    for m in moves:
+        d = m[0]
+        inc = int(m[1:])
+        if d == 'U':
+            for i in range(0, inc):
+                steps += 1
+                newpos = (pos[0], pos[1] + 1)
+                check_colision(grid, colisions, newpos, pnum, steps)
+                pos = newpos 
+        elif d == 'D':
+            for i in range(0, inc):
+                steps += 1
+                newpos = (pos[0], pos[1] - 1)
+                check_colision(grid, colisions, newpos, pnum, steps)
+                pos = newpos 
+        elif d == 'L':
+            for i in range(0, inc):
+                steps += 1
+                newpos = (pos[0] - 1, pos[1])
+                check_colision(grid, colisions, newpos, pnum, steps)
+                pos = newpos  
+        elif d == 'R':
+            for i in range(0, inc):
+                steps += 1
+                newpos = (pos[0] + 1, pos[1])
+                check_colision(grid, colisions, newpos, pnum, steps)
+                pos = newpos
+    pnum += 1 
+
+dist = min( [distance((x[0].x, x[1].y), (0,0)) for x in colisions] )
+print(dist)
+
+st = []
+for c in colisions:
+    st.append(c[0].steps + c[1].steps)
+
+print (min(st))
+
+f.close()
