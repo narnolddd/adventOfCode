b926c5722f3e7dda213c33d1015ec5cc07d7190d
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2019-12-09 23:16:56 +0000

    Naomi slowly catching up

---
+++ 2019/Day8/day8Naomi.py
@@ -0,0 +1,36 @@
+from collections import Counter
+
+file="Day8/inputnaomi.txt"
+with open(file,'r') as f:
+    imagedata=f.read().strip()
+
+w, h = 25, 6
+size = w * h
+
+layers = [imagedata[size*i:size*(i+1)] for i in range(int(len(imagedata)/size))]
+
+zero_counts = [Counter(layer)['0'] for layer in layers]
+layer_index = zero_counts.index(min(zero_counts))
+
+ones, twos = Counter(layers[layer_index])['1'], Counter(layers[layer_index])['2']
+print("Part 1: "+str(ones*twos))
+
+def concat_layer(top,bot):
+    top, bot = int(top),  int(bot)
+    if top==0 or top ==1:
+        return top
+    return bot
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+top_layer=[l for l in layers[0]]
+for k in range(len(layers)):
+    new_layer=[concat_layer(top_layer[m],layers[k][m]) for m in range(150)]
+    top_layer=new_layer
+
+
+
+image_negative= np.array(new_layer).reshape(6,25)
+plt.imshow(image_negative)
+plt.show()
