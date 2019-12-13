7029c76f70a8b945a6a54aef085e2ed456063448
Richard G. Clegg <richard@richardclegg.org>
2019-12-13 23:57:43 +0000

    Oh my god I so hate intcode

--- 2019/Day12/rgc.py
+++ 2019/Day12/rgc.py
@@ -1,55 +1,117 @@
 #!/usr/bin/env python3
 import sys
 import math
+from itertools import permutations 
 
-def parsePlanet(string):
-    string=string[1:len(string)-2]
-    coords=string.split(",")
-    #print(coords)
-    xco=int(coords[0][2:])
-    yco=int(coords[1][3:])
-    zco=int(coords[2][3:])
-    #print(xco,yco,zco)
-    return([xco,yco,zco,0,0,0])
+def calcVal(s,pos,mode,rbase):
+    if mode == 0:
+        return s[s[pos]]
+    if mode == 1:
+        return s[pos]
+    if mode == 2:
+        return s[rbase+s[pos]]
+    print("Unknown mode",mode)
+    sys.exit()
 
+def putVal(s,pos,mode,rbase,val):
+    if mode == 0:
+        s[s[pos]]= val
+        return
+    if mode == 1:
+        print("Mode 1 put should not happen")
+        s[pos]= val
+    if mode == 2:
+        #print("Mode 2 put should not happen")
+        s[rbase+s[pos]]= val
+        return
+    print("Unknown mode",mode)
+    sys.exit()
+
+def doMachine(s,rbase,inp):
+    output= []
+    pos= 0
+    while(True):
+        instr=s[pos]%100
+        mode1= (int(s[pos]/100))%10
+        mode2= (int(s[pos]/1000))%10
+        mode3= (int(s[pos]/10000))%10
+        #print(pos,s[pos],instr,mode1,mode2,mode3)
+        if (instr == 99):
+            return (output,rbase,pos)
+        elif (instr == 1): #add
+            x=calcVal(s,pos+1,mode1,rbase)
+            y=calcVal(s,pos+2,mode2,rbase)
+            putVal(s,pos+3,mode3,rbase,x+y)            
+            pos+=4
+        elif (instr == 2): #mult
+            x=calcVal(s,pos+1,mode1,rbase)
+            y=calcVal(s,pos+2,mode2,rbase)
+            putVal(s,pos+3,mode3,rbase,x*y)            
+            pos+=4
+                #print("Mult",res)
+        elif (instr == 3): #input
+            if inp == False:
+                print("Out of input")
+            putVal(s,pos+1,mode1,rbase,inp)
+            inp=False
+            pos+=2
+        elif (instr == 4): #output
+            out=calcVal(s,pos+1,mode1,rbase)
+            #print("Output",out)
+            output.append(out)
+            pos+= 2
+        elif (instr == 5): 
+            x=calcVal(s,pos+1,mode1,rbase)
+            y=calcVal(s,pos+2,mode2,rbase)
+            if (x != 0):
+                pos= y
+            else:
+                pos+=3
+        elif (instr == 6):
+            x=calcVal(s,pos+1,mode1,rbase)
+            y=calcVal(s,pos+2,mode2,rbase)
+            if (x == 0):
+                pos= y
+            else:
+                pos+=3
+        elif (instr == 7):
+            x=calcVal(s,pos+1,mode1,rbase)
+            y=calcVal(s,pos+2,mode2,rbase)
+            if (x < y):
+                putVal(s,pos+3,mode3,rbase,1)        
+            else:
+                putVal(s,pos+3,mode3,rbase,0)        
+            pos+=4
+        elif (instr == 8):
+            x=calcVal(s,pos+1,mode1,rbase)
+            y=calcVal(s,pos+2,mode2,rbase)
+            if (x == y):
+                putVal(s,pos+3,mode3,rbase,1)        
+            else:
+                putVal(s,pos+3,mode3,rbase,0)        
+            pos+=4
+        elif (instr == 9):
+            rbase+=calcVal(s,pos+1,mode1,rbase)
+            #print("rbase=",rbase)
+            pos+=2
+        else:
+            print("Did not expect ",s[pos])
+            sys.exit()
+    return(output)
+    #print(s)
+    #print("Part 1",s[0])
+    
 fp= open("rgcinput.txt","r")
-strings= fp.readlines()
+l= fp.readline()
 fp.close()
-planets=[]
-for s in strings:
-    planets.append(parsePlanet(s))
-n= len(planets)
-#for p in planets:
-#    print(p)
-for l in range(1000):
-    for i in range(n):
-        for j in range(i):
-            for k in range(3):
-                if planets[i][k] < planets[j][k]:
-                    planets[i][k+3]+= 1
-                    planets[j][k+3]-= 1
-                if planets[i][k] > planets[j][k]:
-                    planets[i][k+3]-= 1
-                    planets[j][k+3]+= 1
-    for i in range(n):
-        for k in range(3):
-            planets[i][k]+= planets[i][k+3]
-#    print(l)
-#    for p in planets:
-#        print(p)
-energy= 0
-for p in planets:
-    potenergy=0
-    kinenergy=0
-    for k in range(3):
-        if (p[k] < 0):
-            potenergy-= p[k]
-        else:
-            potenergy+= p[k]
-    for k in range(3,6):
-        if (p[k] < 0):
-            kinenergy-= p[k]
-        else:
-            kinenergy+= p[k]
-    energy+=potenergy*kinenergy
-print("part1",energy)
+s=[]
+for string in l.split(","):
+    s.append(int(string))
+#print(s)
+for i in range(10000):
+    s.append(0)
+pos= 0
+rbase= 0
+print(doMachine(s,rbase,2)[0])
+
+
