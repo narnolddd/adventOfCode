83eed789efe0d3baf97c80a1648b68dcfef14733
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2019-12-14 14:29:59 +0000

    still not working

--- 2019/Day9/day9Naomi.py
+++ 2019/Day9/day9Naomi.py
@@ -9,22 +9,17 @@ def advance_ic(inputs,ip,ic,rb):
         #print(ic[:20])
         instruction=("0000"+str(ic[ip]))[-5:]
         p1, p2, p3 = [instruction[2-i] for i in range(3)]
+        adr1, adr2, adr3 = 0,0,0
 
         # first parameter
         if ip+1 >= len(ic):
             ic = increase_by(ic, ip+1)
         if p1 == "0":
-            if ic[ip+1]>= len(ic):
-                u1=0
-            else:
-                u1 = ic[ic[ip+1]]
+            adr1 = ic[ip+1]
         elif p1 == "1":
-            u1 = ic[ip+1]
+            adr1 = ip+1
         elif p1 == "2":
-            if rb + ic[ip+1]>=len(ic):
-                u1 = 0
-            else:
-                u1 = ic[rb+ic[ip+1]]
+            adr1 = rb+ic[ip+1]
         else:
             print("unknown position mode "+p1)
 
@@ -32,76 +27,69 @@ def advance_ic(inputs,ip,ic,rb):
         if ip+2 >= len(ic):
             ic = increase_by(ic, ip+2)
         if p2 == "0":
-            if ic[ip+2]>= len(ic):
-                u2=0
-            else:
-                u2 = ic[ic[ip+2]]
+            adr2 = ic[ip+2]
         elif p2 == "1":
-            u2 = ic[ip+2]
+            adr2 = ip+2
         elif p2 == "2":
-            if rb+ic[ip+2]>=len(ic):
-                u2 = 0
-            else:
-                u2 = ic[rb+ic[ip+2]]
+            adr2 = rb+ic[ip+2]
         else:
             print("unknown position mode "+p2)
 
         #third parameter
         if ip+3 >= len(ic):
             ic = increase_by(ic, ip+3)
-        u3 = ic[ip+3]
+        adr3 = ic[ip+3]
 
         op=instruction[3:]
         #print(op)
         if op=="01":
-            if u3>len(ic):
-                ic=increase_by(ic,u3)
-            ic[u3]=u1 + u2
+            if adr3>len(ic):
+                ic=increase_by(ic,adr3)
+            ic[adr3]=ic[adr1] + ic[adr2]
             ip = ip+4
         elif op=="02":
-            if u3>len(ic):
-                ic=increase_by(ic,u3)
-            ic[u3]=u1 * u2
+            if adr3>len(ic):
+                ic=increase_by(ic,adr3)
+            ic[adr3]=ic[adr1] * ic[adr2]
             ip = ip+4
         elif op=="03":
-            if u1>=len(ic):
-                ic=increase_by(ic,u1)
-            ic[u1]=inputs.pop()
-            #print(inp,u1)
+            if adr1>=len(ic):
+                ic=increase_by(ic,adr1)
+            ic[adr1]=inputs.pop()
             ip = ip+2
         elif op=="04":
-            print(u1)
+            print(ic[adr1])
             ip = ip+2
         elif op=="05":
-            if u1!=0:
-                ip=u2
+            if ic[adr1]!=0:
+                ip=ic[adr2]
             else:
                 ip = ip+3
         elif op=="06":
-            if u1==0:
-                ip=u2
+            if ic[adr1]==0:
+                ip=ic[adr2]
             else:
                 ip = ip+3
         elif op=="07":
-            if u3>len(ic):
-                ic=increase_by(ic,u3)
-            if u1<u2:
-                ic[u3]=1
+            if adr3>len(ic):
+                ic=increase_by(ic,adr3)
+            if ic[adr1]<ic[adr2]:
+                ic[adr3]=1
                 ip=ip+4
             else:
-                ic[u3]=0
+                ic[adr3]=0
                 ip=ip+4
         elif op=="08":
-            if u3>len(ic):
-                ic=increase_by(ic,u3)
-            if u1==u2:
-                ic[u3]=1
+            if adr3>len(ic):
+                ic=increase_by(ic,adr3)
+            if adr1==adr2:
+                ic[adr3]=1
                 ip=ip+4
             else:
-                ic[u3]=0
+                ic[adr3]=0
                 ip=ip+4
         elif op=="09":
-            rb = rb + u1
+            rb = rb + ic[adr1]
             #print("RB "+str(rb))
             ip = ip + 2
         else:
