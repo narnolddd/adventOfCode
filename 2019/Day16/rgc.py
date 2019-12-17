319a203c6a0c771ea4b6f9e53c9ad227ac0db8ce
Richard G. Clegg <richard@richardclegg.org>
2019-12-17 00:37:38 +0000

    RGC solution day 16

--- 2019/Day16/rgc.py
+++ 2019/Day16/rgc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import sys,math,numpy
+import sys,math,numpy,fractions
 from itertools import permutations 
 
 def getpat(offset,itern,pat):
@@ -7,7 +7,7 @@ def getpat(offset,itern,pat):
     #print(poff,end=" ")
     return pat[poff%len(pat)]
 
-fp= open("rgctest2.txt","r")
+fp= open("rgcinput.txt","r")
 l= fp.readline().strip()
 fp.close()
 s=[]
@@ -20,23 +20,63 @@ sc= s.copy()
 for i in range(100):
     for x in range(n):
         sumctr= 0
-        for y in range(n):
-            sumctr+=s[y]*getpat(y,x,pattern)
+        pos= x
+        lctr=0
+        #sumctr2=0
+        #for y in range(n):
+        #    sumctr2+= s[y]*pattern[getpat(y,x,pattern)]
+        while pos < n:
+            sumctr+=s[pos]
+            if pos + (2*(x+1)) < n:
+                sumctr-=s[pos + (2*(x+1))]
+            #print ("add pos",pos,"sub pos",pos+2*(x+1))
+            if (lctr == x):
+                lctr= 0
+                pos+=3*(x+1)+1
+            else:
+                pos+=1
+                lctr+=1
+
             #print(getpat(y,x,pattern),end=",")
         #print()
+        #print (x,sumctr,sumctr2)
         if (sumctr < 0):
             sumctr=-sumctr
         snew[x]=(sumctr%10)
     s=snew.copy()
-
+    #for i in s:
+    #    print((i),end="")
+    #print()
 answer=0
 for i in range(8):
     answer*=10
-    answer+=s[i]
+    if s[i]<0:
+        answer+=(-s[i]%10)
+    else:
+        answer+=(s[i]%10)
 print("Part 1",answer)
-sl=[]
 offset=0
 for i in range(7):
     offset*=10
     offset+=sc[i]
-print("Part 2",answer)
+print(n*10000,offset,n*10000-offset)
+s=[]
+for i in range(offset,n*10000):
+    s.append(sc[i%n])
+n=(len(s))
+snew=[0]*n
+for i in range(100):
+    print(i)
+    snew[n-1]=s[n-1]
+    for x in range(n-2,-1,-1):
+        snew[x]=snew[x+1]+s[x]
+    for x in range(n):
+        s[x]=snew[x]%10 
+answer=0
+for i in range(8):
+    answer*=10
+    if s[i]<0:
+        answer+=(-s[i]%10)
+    else:
+        answer+=(s[i]%10)
+print(answer)
