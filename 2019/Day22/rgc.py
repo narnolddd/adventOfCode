ca5dcc18c43c1d3fcc707036117272ebf4344be2
Richard G. Clegg <richard@richardclegg.org>
2019-12-24 08:09:33 +0000

    Day 24 recursive game of life RGC

--- 2019/Day22/rgc.py
+++ 2019/Day22/rgc.py
@@ -3,19 +3,12 @@ import sys,math,numpy
 from itertools import permutations 
 
 def reverseCut(n,pos,decklen):
-    if n >= 0:
-        if pos > decklen-1-n:
-            return pos-(decklen-1-n)
-        return pos+n
-    if pos > decklen-1+n:
-        return pos+n
-    return pos-(decklen-1+n)
-            
+    return (pos+n)%decklen
+    
 
 def reverseIncrement(n,pos,decklen):
-    pos+=decklen*pos/(decklen-n)
-    pos/=n
-    return pos
+    npos=floor(pos/n)
+    return npos
     
 def reverseStack(pos,decklen):
     return decklen-1-pos
@@ -30,11 +23,9 @@ def increment(n,deck):
     pos=0
     
     for i in range(len(d)):
-        deck[pos]=d[i]
-        pos+=n
-        while pos >=len(deck):
-            pos-= len(deck)
-    assert sum(deck) == sum(d)
+        deck[(i*n)%len(d)]=d[i]
+        #pos= (pos+n)%len(deck)
+
     return deck
     
 def stack(deck):
@@ -47,10 +38,12 @@ if len(sys.argv) != 2:
 fp= open(sys.argv[1],"r")
 lines= fp.readlines()
 fp.close()
+decklen=10007
+deck=list(range(decklen))
 
-deck=list(range(10007))
 s=sum(deck)
 for l in lines:
+    print(deck.index(2019),end=" ")
     l=l.split(" ")
     if (l[0] == "cut"):
         deck= cut(int(l[1]),deck)
@@ -65,27 +58,40 @@ for l in lines:
     else:
         print("Don't know shuffle",l)
 #print(deck)
-print(deck.index(2019))
+print("Part 1",deck.index(2019))
 
-d2=list(range(10))
-d2=increment(7,d2)
-print(d2)
-for i in range(10):
-    print(reverseIncrement(7,d2[i],10))
-sys.exit()
-decklen=100007
-pos=6978
-for l in reversed(lines):
-    l=l.split(" ")
-    if (l[0] == "cut"):
-        pos= reverseCut(int(l[1]),pos,decklen)
-    elif l[0] == "deal":
-        if l[1] == "with":
-            pos= reverseIncrement(int(l[3]),pos,decklen)
-        elif l[1] == "into":
-            pos= reverseStack(pos,decklen)
+pos=reverseCut(-9110,6978,decklen)
+print("Back one step",pos)
+pos=reverseIncrement(70,pos,decklen)
+print("Back two steps",pos)
+
+pos= 6978
+plist=[pos]
+i=0
+while True:
+    for l in reversed(lines):
+        print(pos,end=" ")
+        l=l.split(" ")
+        if (l[0] == "cut"):
+            pos= reverseCut(int(l[1]),pos,decklen)
+        elif l[0] == "deal":
+            if l[1] == "with":
+                pos= reverseIncrement(int(l[3]),pos,decklen)
+            elif l[1] == "into":
+                pos= reverseStack(pos,decklen)
+            else:
+                print("Don't know deal")
+                sys.exit()
         else:
-            print("Don't know deal")
-            sys.exit()
-    else:
-        print("Don't know shuffle",l)
+            print("Don't know shuffle",l)
+        
+    sys.exit()
+    if pos in plist:
+        print("Got loop at ",i,plist.index(pos))
+        print(plist[1])
+        sys.exit()
+    plist.append(pos)
+    print(pos)
+    i+=1
+    
+print(pos)
