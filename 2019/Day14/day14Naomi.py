fee7e88a1df0618ad4bc21ab008a2730cd7c1491
Naomi Arnold <n.a.arnold@qmul.ac.uk>
2019-12-30 16:39:29 +0000

    Done day 14

--- 2019/Day14/day14Naomi.py
+++ 2019/Day14/day14Naomi.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 class Stack:
      def __init__(self):
          self.items = []
@@ -16,3 +19,78 @@ class Stack:
 
      def size(self):
          return len(self.items)
+
+reactions = {}
+index = {}
+products = {}
+
+file = "Day14/inputnaomi.txt"
+ind=0
+
+with open(file,'r') as f:
+    for _, line in enumerate(f):
+        ingredients, product = line.split("=>")
+        product = product.strip()
+        parts = ingredients.split(',')
+
+        amount, el = product.split()
+        index[el]=ind
+        products[ind]=el
+        reactions[el] = [int(amount), [p.strip() for p in parts]]
+        ind+=1
+
+needed=np.zeros(len(index.keys()),dtype=int)
+needed[index['FUEL']]=1
+ore=0
+
+while True:
+    if np.all(needed<=0):
+        break
+    for i in range(len(needed)):
+        if needed[i]>0:
+            el, amount = products[i], math.ceil(float(needed[i])/reactions[products[i]][0])
+            for part in reactions[el][1]:
+                p = part.split()
+                am, ing = int(p[0]), p[1]
+                if ing == "ORE":
+                    ore+=am*amount
+                    continue
+                needed[index[ing]]+=am*amount
+            needed[i]-=reactions[products[i]][0]*amount
+
+part1_ore = ore
+print("Part 1: "+str(part1_ore))
+
+def how_many_ore(fuel):
+    needed=np.zeros(len(index.keys()),dtype=int)
+    needed[index['FUEL']]=fuel
+    ore = 0
+    while True:
+        if np.all(needed<=0):
+            break
+        for i in range(len(needed)):
+            if needed[i]>0:
+                el, amount = products[i], math.ceil(float(needed[i])/reactions[products[i]][0])
+                for part in reactions[el][1]:
+                    p = part.split()
+                    am, ing = int(p[0]), p[1]
+                    if ing == "ORE":
+                        ore+=am*amount
+                        continue
+                    needed[index[ing]]+=am*amount
+                needed[i]-=reactions[products[i]][0]*amount
+    return ore
+
+init_guess = int(1000000000000/part1_ore)
+guesses = range(init_guess,2*init_guess)
+
+while True:
+    lb, ub, mb, = guesses[0], guesses[len(guesses)-1], guesses[int(len(guesses)/2)]
+    if lb == mb or mb == ub:
+        break
+    if how_many_ore(mb)<=1000000000000:
+        guesses = range(mb,ub)
+    else:
+        guesses = range(lb,mb)
+
+print("Part 2: "+str(mb))
---
+++ 2019/Day23/day23Naomi.py
@@ -0,0 +1,125 @@
+def increase_by(ic,size):
+    increase = size + 1 - len(ic)
+    new_ic = ic+[0 for _ in range(increase)]
+    ic = new_ic
+    return ic
+
+def advance_ic(inputs,ip,ic,rb):
+    outputs=[]
+    while ic[ip]!=99:
+        #print(ic)
+        #print(ic[:20])
+        instruction=("0000"+str(ic[ip]))[-5:]
+        p1, p2, p3 = [instruction[2-i] for i in range(3)]
+
+        # first parameter
+        if ip+1 >= len(ic):
+            ic = increase_by(ic, ip+1)
+        if p1 == "0":
+            adr1 = ic[ip+1]
+        elif p1 == "1":
+            adr1 = ip+1
+        elif p1 == "2":
+            adr1 = rb+ic[ip+1]
+        else:
+            print("unknown position mode "+p1)
+
+        # second parameter
+        if ip+2 >= len(ic):
+            ic = increase_by(ic, ip+2)
+        if p2 == "0":
+            adr2 = ic[ip+2]
+        elif p2 == "1":
+            adr2 = ip+2
+        elif p2 == "2":
+            adr2 = rb+ic[ip+2]
+        else:
+            print("unknown position mode "+p2)
+
+        #third parameter
+        if ip+3 >= len(ic):
+            ic = increase_by(ic, ip+3)
+        adr3 = ic[ip+3]
+        if p3 == "1":
+            adr3 = ip+3
+        if p3 == "2":
+            adr3 = rb + ic[ip+3]
+
+        op=instruction[3:]
+        #print(op)
+        if op=="01":
+            if adr3>=len(ic):
+                ic=increase_by(ic,adr3)
+            u1, u2 = 0,0
+            if adr1<len(ic):
+                u1=ic[adr1]
+            if adr2<len(ic):
+                u2=ic[adr2]
+            ic[adr3]=u1 + u2
+            ip = ip+4
+        elif op=="02":
+            if adr3>=len(ic):
+                ic=increase_by(ic,adr3)
+            u1, u2 = 0,0
+            if adr1<len(ic):
+                u1=ic[adr1]
+            if adr2<len(ic):
+                u2=ic[adr2]
+            ic[adr3]=u1*u2
+            ip = ip+4
+        elif op=="03":
+            if adr1>=len(ic):
+                ic=increase_by(ic,adr1)
+            if len(inputs)==0:
+                ic[adr1]=-1
+            else:
+                ic[adr1]=inputs.pop()
+            ip = ip+2
+        elif op=="04":
+            ip = ip+2
+            outputs.append(ic[adr1])
+            if len(outputs)==3:
+                return outputs, ic, ip, rb
+        elif op=="05":
+            if ic[adr1]!=0:
+                ip=ic[adr2]
+            else:
+                ip = ip+3
+        elif op=="06":
+            if ic[adr1]==0:
+                ip=ic[adr2]
+            else:
+                ip = ip+3
+        elif op=="07":
+            if adr3>len(ic):
+                ic=increase_by(ic,adr3)
+            if ic[adr1]<ic[adr2]:
+                ic[adr3]=1
+                ip=ip+4
+            else:
+                ic[adr3]=0
+                ip=ip+4
+        elif op=="08":
+            if adr3>len(ic):
+                ic=increase_by(ic,adr3)
+            if ic[adr1]==ic[adr2]:
+                ic[adr3]=1
+                ip=ip+4
+            else:
+                ic[adr3]=0
+                ip=ip+4
+        elif op=="09":
+            #print(rb)
+            rb = rb + ic[adr1]
+            #print("RB "+str(rb))
+            ip = ip + 2
+        else:
+            print('error: instruction reads '+instruction)
+            break
+    return None, ic, ip, rb
+
+class Node():
+    def __init__(self, address):
+        self.address = address
+        self.packetqueue = []
+        self.firstrun = True
