54c1d47c6d9c94a6d13e6c063f0d03edfb6e2099
ederlf <ederleaofernandes@gmail.com>
2020-12-09 13:48:03 +0000

    Damn, tabs indentation, just no

--- 2020/Day09/ed9.py
+++ 2020/Day09/ed9.py
@@ -11,14 +11,14 @@ comb_sum = set(map(lambda x: sum(x), itertools.combinations(preamble, 2)))
 i = 0
 invalid_num = None
 for n in numbers[preamble_size:]:
-	if n not in comb_sum:
-		invalid_num = n
-		break
-	preamble[i%preamble_size] = n
-	# I could update only the new sum, but I would have to remove the old one
-	# Not today, this is fast enough.
-	comb_sum = set(map(lambda x: sum(x), itertools.combinations(preamble, 2)))
-	i += 1
+    if n not in comb_sum:
+        invalid_num = n
+        break
+    preamble[i%preamble_size] = n
+    # I could update only the new sum, but I would have to remove the old one
+    # Not today, this is fast enough.
+    comb_sum = set(map(lambda x: sum(x), itertools.combinations(preamble, 2)))
+    i += 1
 
 print(invalid_num)
 
@@ -26,17 +26,17 @@ print(invalid_num)
 possible = [n for n in numbers if n < invalid_num]
 
 def solve2(possible):
-	i = 0
-	num = 0
-	while True:
-		for n in range(i, len(possible)):
-			num += numbers[n]
-			if num == invalid_num:
-				return max(possible[i:n]) + min(possible[i:n])
-			if num > invalid_num:
-				break
-		num = 0
-		i += 1
+    i = 0
+    num = 0
+    while True:
+        for n in range(i, len(possible)):
+            num += numbers[n]
+            if num == invalid_num:
+                return max(possible[i:n]) + min(possible[i:n])
+            if num > invalid_num:
+                break
+        num = 0
+        i += 1
 
 print(solve2(possible))
 
